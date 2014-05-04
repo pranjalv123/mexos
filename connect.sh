edbace7abe133e9c3676d3269c55b7a01da3fdca
Pranjal Vachaspati <pranjal@mit.edu>
2014-05-04 17:22:22 -0400

    add two more nodes

--- connect.sh
+++ connect.sh
@@ -1,2 +1,2 @@
 #ssh-keygen -f "/home/pranjal/.ssh/known_hosts" -R 54.86.104.17
-ssh -A -i mexos-keypair.pem ubuntu@54.86.104.17
+ssh -A -i mexos-keypair-1.pem ubuntu@54.86.104.17
--- launch-nodes.sh
+++ launch-nodes.sh
@@ -1,4 +1,4 @@
-for i in {103..110}
+for i in {111..112}
 do
-    ec2-run-instances ami-b7667cde --private-ip-address 10.0.0.$i -t m1.small -g sg-e51d9980 --subnet subnet-563a3d10 -k mexos-keypair --associate-public-ip-address true
+    ec2-run-instances ami-b7667cde --private-ip-address 10.0.0.$i -t m1.small -g sg-e51d9980 --subnet subnet-563a3d10 -k mexos-keypair-1 --associate-public-ip-address true
 done
