dcf822f3eb182e529e51b797b0dba5f8f96be1f7
Pranjal Vachaspati <pranjal@mit.edu>
2014-05-10 14:43:53 -0400

    levigo benchmark

--- levigo-ex.go
+++ levigo-ex.go
@@ -1,8 +1,20 @@
 package main
 import "github.com/jmhodges/levigo"
 import "fmt"
+import "math/rand"
+import "time"
+func trace(s string) (string, time.Time) {
+    fmt.Println("START:", s)
+    return s, time.Now()
+}
 
-func main() {
+func un(s string, startTime time.Time) {
+    endTime := time.Now()
+    fmt.Println("  END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime))
+}
+func db_create() (*levigo.DB) {
+	defer un(trace("db create"))
+
 	opts := levigo.NewOptions()
 	opts.SetCache(levigo.NewLRUCache(3<<30))
 	opts.SetCreateIfMissing(true)
@@ -12,5 +24,37 @@ func main() {
 	db.Put(wo, []byte("key1"), []byte("val1"))
 	data, _ := db.Get(ro, []byte("key1"))
 	fmt.Println(string(data));
+	return db
+}
 
+func gen_data(size uint64) ([]byte) {
+	defer un(trace("gen data"))
+	
+	data := make([]byte, size)
+//	src := new(rand.Rand)
+	for i := range data {
+		data[i] = byte(rand.Int31() & 0xff)
+	}
+	return data
+}
+
+func write_data(data []byte, db *levigo.DB, nitems int, keysize int, valsize int) {
+	defer un(trace("write data"))
+	wo := levigo.NewWriteOptions()	
+	keystart := 0
+	valstart := 0
+	for i:=0; i < nitems; i++ {
+		db.Put(wo, 
+			data[keystart:keystart + keysize], 
+			data[valstart:valstart + valsize])
+		keystart %= (len(data) - keysize)
+		valstart %= (len(data) - valsize)
+	}
+	fmt.Println("Wrote ", nitems, "items with key size", keysize, "and value size", valsize)
+}
+
+func main() {
+	db := db_create()
+	data := gen_data(1 << 24)
+	write_data(data, db, 10000000, 16, 256)
 }
