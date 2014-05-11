1a0a03a16ddababce0891eddbdc019209713e820
Pranjal Vachaspati <pranjal@mit.edu>
2014-05-11 18:52:17 -0400

    update params

--- src/test/test_test.go
+++ src/test/test_test.go
@@ -417,9 +417,9 @@ func paddedRandIntString(size int) string {
 func TestDiskRecovery(t *testing.T) {
 	nclients := 1
 //	numBytes := 20971520 //20MB
-	nItems := 1023
+	nItems := 682
 	keySize := 32
-	valSize := 100 * 1024
+	valSize := 150 * 1024
 	smPorts, gids, kvPorts := setup("basic", false, numGroups, numReplicas)
 	//defer clean()
 	
